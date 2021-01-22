1b0e9bc4ec5d83a2c1d78e91ca5e0ccd28ce6e68
bordumb <brian.s.deely@gmail.com>
2021-01-22 21:45:18 +0000

    cleaned typing

--- trading/maths/maths.py
+++ trading/maths/maths.py
@@ -25,13 +25,13 @@ def divide_kernel(numerator: float, denominator: float) -> float:
     return numerator / denominator
 
 
-def divide(df: 'DataFrame', numerator_col: float, denominator_col: float) -> 'DataFrame':
+def divide(df: 'DataFrame', numerator_col: str, denominator_col: str) -> 'DataFrame':
     """Divides one number by another.
 
     Args:
         df: the dataframe to append a column onto
-        numerator_col: the number for the top of your fraction
-        denominator_col: the number for the bottom of your fraction
+        numerator_col: the name of your numerator column
+        denominator_col: the name of your denominator column
 
     Returns:
         the original dataframe with the division result appended
