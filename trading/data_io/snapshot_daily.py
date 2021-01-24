aa57e3277d6fb9ee05befc8d13fba11e5988d51a
bordumb <brian.s.deely@gmail.com>
2021-01-24 00:58:10 +0000

    completed tests for maths

--- trading/data_io/snapshot_daily.py
+++ trading/data_io/snapshot_daily.py
@@ -3,10 +3,7 @@ from typing import Optional, List
 from abc import abstractmethod
 import csv
 import pandas
-import pandas.io.data as web
 import os
-
-os.chdir('trading/data/')
 
 
 class CSV:
@@ -23,8 +20,8 @@ class CSV:
         pass
 
 
-def create_daily_snapshot(start_dt: str, end_dt: str, file_name: Optional[str] = 'stock_tickers.csv',
-                          stock_tickers: Optional[List[str]] = None) -> CSV:
+def write_snapshot(start_dt: str, end_dt: str, file_name: Optional[str] = 'stock_tickers.csv',
+                   stock_tickers: Optional[List[str]] = None) -> CSV:
     """Takes in a csv of stock tickers and outputs financial data.
 
     Args:
@@ -43,9 +40,10 @@ def create_daily_snapshot(start_dt: str, end_dt: str, file_name: Optional[str] =
     .. code-block:: python
 
         from trading.data_io import snapshot_daily
-        snapshot_daily.create_daily_snapshot(start_dt='2021-01-01', end_dt='2021-01-01')
-        snapshot_daily.create_daily_snapshot(start_dt='2017-01-01', end_dt='2017-04-30', stock_tickers=['AAPL', 'GME'])
+        snapshot_daily.write_snapshot(start_dt='2021-01-01', end_dt='2021-01-01')
+        snapshot_daily.write_snapshot(start_dt='2017-01-01', end_dt='2017-04-30', stock_tickers=['AAPL', 'GME'])
     """
+    os.chdir('trading/data/')
     # TODO: swap out yfinance for something else
     if stock_tickers is None:
         df = pandas.read_csv(file_name)
@@ -58,14 +56,70 @@ def create_daily_snapshot(start_dt: str, end_dt: str, file_name: Optional[str] =
             data.to_csv(f'prices/snapshot/daily/{ticker}.csv')
 
 
-from pandas_datareader import data
+def read_snapshot(stock_tickers: Optional[List[str]] = None) -> 'Dataframe':
+    """Reads all, some, or one specific stock's financial snapshot dataset.
 
-# Only get the adjusted close.
-aapl = data.DataReader("AAPL",
-                       start='2020-1-1',
-                       end='2020-1-1',
-                       data_source='nasdaq')['Adj Close']
+    Args:
+        stock_tickers: the name(s) of the stocks you want to pull
 
+    Returns:
+        one csv per each stock ticker in the input file
 
-data.DataReader(ticker, 'google', start_date, end_date)
+    Note:
+        this may create many files on your computer
+
+    **Example**
+
+    .. code-block:: python
+
+    from trading.data_io import snapshot_daily
+    snapshot_daily.read_snapshot()
+    dict_of_df = snapshot_daily.read_snapshot(stock_tickers=['AAPL'])
+    """
+    dataframes = {}
+    base_path = 'trading/data/prices/snapshot/daily/'
+    files = [file_name for file_name in os.listdir(base_path)]
+    if stock_tickers is not None:
+        # Pull only the files that the user specifies
+        select_files = [file_name for file_name in os.listdir(base_path)
+                 if any(file_name == stock + '.csv' for stock in stock_tickers)]
+        for file_name in select_files:
+            symbol = file_name.split(".")[0]
+            df = pandas.read_csv(f'{base_path}{file_name}')
+            dataframes[symbol] = df
+            return dataframes
+    else:
+        for file_name in files:
+            symbol = file_name.split(".")[0]
+            df = pandas.read_csv(f'{base_path}{file_name}')
+            dataframes[symbol] = df
+            return dataframes
+
+#
+#
+# import os
+# import pandas
+#
+# stock_tickers = ['AAPL']
+# dataframes = {}
+# base_path = 'trading/data/prices/snapshot/daily/'
+#
+# if stock_tickers is not None:
+#     # Pull only the files that the user specifies
+#     select_files = [file_name for file_name in os.listdir(base_path)
+#                     if any(file_name == stock + '.csv' for stock in stock_tickers)]
+#     for file_name in select_files:
+#         symbol = file_name.split(".")[0]
+#         df = pandas.read_csv(f'{base_path}{file_name}')
+#         dataframes[symbol] = df
+#         dataframes
+#
+#
+# files = [file_name for file_name in os.listdir(base_path)]
+# for file_name in files:
+#     print(file_name)
+#     symbol = file_name.split(".")[0]
+#     df = pandas.read_csv(f'{base_path}{file_name}')
+#     dataframes[symbol] = df
+#     dataframes
 
