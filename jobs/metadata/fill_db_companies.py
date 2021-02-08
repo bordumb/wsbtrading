864d9e7c5a27fef5a97a81392ff1657be6f6669c
bordumb <brian.s.deely@gmail.com>
2021-02-08 23:03:25 +0000

    updated wheel, postgres query

--- jobs/metadata/fill_db_companies.py
+++ jobs/metadata/fill_db_companies.py
@@ -1,4 +1,5 @@
 import csv
+import psycopg2
 
 from wsbtrading.data_io import data_io
 
@@ -15,40 +16,91 @@ from wsbtrading.data_io import data_io
 
 conn, cur = data_io.postgres_conn()
 
+csv_path = f'../../data/prod/stock_tickers/daily/date=2021-02-07/file.csv'
+
+
+def get_table_field_names(table_name: str):
+    """Queries the field names of a given table.
+
+    Args:
+        table_name: the name of the table to query
+
+    Returns:
+        a list of column names
+
+    **Example**
+
+    .. code-block:: python
+
+        mapped_df = get_table_field_names(table_name='company_listing_status')
+    """
+    try:
+        cur.execute(f'Select * FROM {table_name} LIMIT 0')
+        col_names = [desc[0] for desc in cur.description]
+    except psycopg2.Error as e:
+        print(f'error: {e}')
+
+    return col_names
+
+
+def insert_csv_to_sql(table_name: str, csv_path: str, delimiter: str = ','):
+    """Given a SQL table.
+
+    Args:
+        table_name: the name of the table to query
+        csv_path: the path to the data file to import
+        delimiter: the type of delimiter for the file
+
+    **Example**
+
+    .. code-block:: python
+
+        insert_csv_to_sql(table_name='company_listing_status', csv_path='../file.csv')
+    """
+    col_names = get_table_field_names(table_name=table_name)
+
+    try:
+        csv_contents = open(csv_path, 'r')
+        next(csv_contents)
+    except psycopg2.Error as e:
+        print(f'error: {e}')
+
+    try:
+        cur.copy_from(file=csv_contents,
+                      table=table_name,
+                      columns=col_names,
+                      sep=delimiter)
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("Success!")
+    except psycopg2.Error as e:
+        print(f'error: {e}')
+
 
 def main():
     # ---------------
     # Create Table  |
     # ---------------
     cur.execute("""
-        CREATE TABLE company_listing_status(
-        id integer,
-        symbol text PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS company_listing_status(
+        id text,
+        symbol text,
         name text,
         exchange text,
-        assetType text, 
-        ipoDate date, 
-        delistingDate date,
+        assetType text,
+        ipoDate text,
+        delistingDate text,
         status text,
-        date_created date
+        date_created text
     )
     """)
     conn.commit()
 
-    csv_path = f'../../data/prod/stock_tickers/daily/date=*/file.csv'
-
     # ---------------
     # Insert Data   |
     # ---------------
-    with open(csv_path, 'r') as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip the header row.
-        for row in reader:
-            cur.execute(
-                "INSERT INTO company_listing_status VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                row
-            )
-    conn.commit()
+    insert_csv_to_sql(table_name='company_listing_status', csv_path=csv_path)
 
 
 if __name__ == '__main__':
--- wsbtrading/__init__.py
+++ wsbtrading/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0a08'
+__version__ = '1.0.0a09'
 
 __doc__ = """wsbtrading is a library that handles data I/O, aggregation, and modeling to facilitate algorithmic trading stategies."""
 
