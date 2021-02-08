ac16fa75fd3448d561bb2fe620e7da38812baf9b
bordumb <brian.s.deely@gmail.com>
2021-02-08 23:07:30 +0000

    updated postgres query

--- jobs/metadata/fill_db_companies.py
+++ jobs/metadata/fill_db_companies.py
@@ -3,16 +3,6 @@ import psycopg2
 
 from wsbtrading.data_io import data_io
 
-# alpaca_api = data_io.alpaca_rest_api_conn(trading_type='paper_trading')
-#
-# minute_bars = alpaca_api.polygon.historic_agg_v2(symbol='Z',
-#                                                  multiplier=1,
-#                                                  timespan='minute',
-#                                                  _from='2020-10-02',
-#                                                  to='2020-10-22')
-#
-# for bar in minute_bars:
-#     print(bar.timestamp, bar.open, bar.high, bar.low, bar.close)
 
 conn, cur = data_io.postgres_conn()
 
